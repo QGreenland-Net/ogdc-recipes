82fb47cbfe7bc5d67a255e904098b1b2edd9fb89
Trey Stafford <trey.stafford@colorado.edu>
2024-12-11 17:35:05 -0700

    Support for ogdc-runner utilization of argo/hera.
    * Replace `{input_dir}` and `{output_dir}` slugs with `/input_dir/` an
      `/output_dir/`. These are the actual mount locations of data for each command
      that ogdc uses, managed by argo.
    * Fixup broken recipe for sea-ice-conc

--- recipes/seal-tags/recipe.sh
+++ recipes/seal-tags/recipe.sh
@@ -1,4 +1,5 @@
 # WARNING: This is not a real shell script, although it is meant to look and
 # feel like one. Each line, unless it starts with #, is interpreted as a
 # workflow step.
-ogr2ogr -lco ENCODING=UTF-8 -t_srs "EPSG:3413" -makevalid -s_srs "EPSG:4326" -oo X_POSSIBLE_NAMES=Longitude -oo Y_POSSIBLE_NAMES=Latitutde {output_dir}/ct71_ODV_epsg3413.gpkg {input_dir}/ct71_ODV.csv
+
+ogr2ogr -lco ENCODING=UTF-8 -t_srs "EPSG:3413" -makevalid -s_srs "EPSG:4326" -oo X_POSSIBLE_NAMES=Longitude -oo Y_POSSIBLE_NAMES=Latitutde /output_dir/ct71_ODV_epsg3413.gpkg /input_dir/ct71_ODV.csv
