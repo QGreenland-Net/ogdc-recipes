d710175285ec8554627ef27c7e439f4e98a3b7de
rmarow <robyn.marowitz@colorado.edu>
2026-02-05 10:35:26 -0700

    fix variable issue!

--- recipes/greenland-ice-sheet/recipe.sh
+++ recipes/greenland-ice-sheet/recipe.sh
@@ -2,4 +2,4 @@
 # feel like one. Each line, unless it starts with #, is interpreted as a
 # workflow step.
 
-bash -c 'mkdir -p /output_dir && for nc_file in /input_dir/*.nc; do base_name=$(basename "$nc_file" .nc); echo "Processing $nc_file..."; gdalwarp -t_srs EPSG:3413 -of GTiff -co COMPRESS=LZW "NETCDF:${nc_file}:percent_contribution" "/output_dir/${base_name}_epsg3413.tif"; done && echo "Complete! Created $(ls /output_dir/*.tif | wc -l) files"'
+bash -c 'mkdir -p /output_dir && for nc_file in /input_dir/*.nc; do base_name=$(basename "$nc_file" .nc); echo "Processing $nc_file..."; gdalwarp -t_srs EPSG:3413 -of GTiff -co COMPRESS=LZW "NETCDF:${nc_file}:sftgif" "/output_dir/${base_name}_epsg3413.tif"; done && echo "Complete! Created $(ls /output_dir/*.tif | wc -l) files"'
