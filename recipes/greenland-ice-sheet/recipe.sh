e119a5955fe7c2f26e445f0368f0e962e2333d0b
rmarow <robyn.marowitz@colorado.edu>
2026-02-05 10:18:34 -0700

    fix error!

--- recipes/greenland-ice-sheet/recipe.sh
+++ recipes/greenland-ice-sheet/recipe.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
+# WARNING: This is not a real shell script, although it is meant to look and
+# feel like one. Each line, unless it starts with #, is interpreted as a
+# workflow step.
 
-for nc_file in /input_dir/*.nc; do
-    base_name=$(basename "$nc_file" .nc)
-    
-    # Convert NetCDF to GeoTIFF with EPSG:3413
-    gdalwarp -t_srs EPSG:3413 \
-        -of GTiff \
-        -co COMPRESS=LZW \
-        "NETCDF:${nc_file}:percent_contribution" \
-        "/output_dir/${base_name}_epsg3413.tif"
-done
+bash -c 'mkdir -p /output_dir && for nc_file in /input_dir/*.nc; do base_name=$(basename "$nc_file" .nc); echo "Processing $nc_file..."; gdalwarp -t_srs EPSG:3413 -of GTiff -co COMPRESS=LZW "NETCDF:${nc_file}:percent_contribution" "/output_dir/${base_name}_epsg3413.tif"; done && echo "Complete! Created $(ls /output_dir/*.tif | wc -l) files"'
