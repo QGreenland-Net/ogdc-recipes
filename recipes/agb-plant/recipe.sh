94c279128ad479ba4b18f3fede31caed834e4bd0
rmarow <robyn.marowitz@colorado.edu>
2026-03-25 11:59:42 -0600

    missing file error fix attempt

--- recipes/agb-plant/recipe.sh
+++ recipes/agb-plant/recipe.sh
@@ -1,4 +1,4 @@
 # Build VRT mosaic from Greenland-relevant tiles (A3, B2, B3)
 gdalbuildvrt /output_dir/plant_agb_2020_p500_merged.vrt /input_dir/plant_agb_2020_p500_*.tif
 # Reproject to EPSG:3413 and clip to QGreenland extent
-gdalwarp -r bilinear -t_srs EPSG:3413 -te -998012.0 -3611178.0 1228670.0 -329624.0 -co COMPRESS=LZW -co TILED=YES /output_dir/plant_agb_2020_p500_merged.vrt /output_dir/plant_agb_2020_p500_greenland.tif
+gdalwarp -r bilinear -t_srs EPSG:3413 -te -998012.0 -3611178.0 1228670.0 -329624.0 -co COMPRESS=LZW -co TILED=YES /input_dir/plant_agb_2020_p500_merged.vrt /output_dir/plant_agb_2020_p500_greenland.tif
